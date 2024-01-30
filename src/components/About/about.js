021df60bbf5ba504dd6a48ded0e93f53cb7a6890
09Riri09 <rares.dascalescu@gmail.com>
2024-01-30 22:59:06 +0200

    m

---
+++ src/components/About/about.js
@@ -0,0 +1,26 @@
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters/animatedletters";
+
+function About() {
+  <div className="container about-page">
+    <div className="text-zone">
+      <h1>
+        <AnimatedLetters
+          strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+          idx={15}
+        />
+      </h1>
+      <p>
+        Passionate front-end developer with a marketing background, eager to
+        dive into the dynamic world of tech. Armed with ambition, I seek
+        opportunities with established IT companies to harness modern
+        technologies and fuel my insatiable drive to learn and excel in the
+        field. Let's craft innovative digital experiences together!
+      </p>
+      <p></p>
+      <p></p>
+    </div>
+  </div>;
+}
+
+export default About;
