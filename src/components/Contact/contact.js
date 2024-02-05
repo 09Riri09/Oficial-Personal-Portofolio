d64598f8085e7049eaddec16ac4852f7fb03ec1c
09Riri09 <rares.dascalescu@gmail.com>
2024-02-05 20:33:14 +0200

    contact form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,12 +1,85 @@
-import './contact.scss';
-
+import "./contact.scss";
+import Loader from "react-loaders";
+import AnimatedLetters from  '../AnimatedLetters/animatedletters'
+import { useState , useEffect , useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const form = useRef()
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+    return () => clearTimeout(timeoutId);
+  }, []);
 
-    return (
-        "hello"
-    )
+  const sendEmail = (e) => {
+    e.preventDefault()
 
+    emailjs
+      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass = {letterClass}
+              strArray={["C", "o", "n", "t", "a", "c", "t ", " " , " m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type = 'ball-scale-ripple-multiple' />
+    </>
+  );
 }
-
 export default Contact;
