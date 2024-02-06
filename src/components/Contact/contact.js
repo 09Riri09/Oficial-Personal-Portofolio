59f2a5e4031ae691122bad175880354e88aa9d11
09Riri09 <rares.dascalescu@gmail.com>
2024-02-06 19:39:45 +0200

    portofolio finished without mobile optimisation

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,6 +3,7 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters/animatedletters";
 import { useState, useEffect, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { MapContainer, TileLayer , Marker , Popup } from "react-leaflet";
 
 function Contact() {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -18,17 +19,19 @@ function Contact() {
     e.preventDefault();
 
     emailjs
-      .sendForm("gmail", "service_kquqgph", form.current, "template_g97cjk4" , 
-      {
-        publicKey: 'YDhYodi8SsQPGJY8G',
-      })
+      .sendForm(
+        "service_kquqgph",
+        "template_eow25qk",
+        form.current,
+        "YDhYodi8SsQPGJY8G"
+      )
       .then(
         () => {
-          alert("Message successfully sent!");
+          alert("Message succesfully snet!");
           window.location.reload(false);
         },
         () => {
-          alert("Failed to send the message, please try again");
+          alert("Failed to send the message, please contact through linkedIn");
         }
       );
   };
@@ -79,6 +82,19 @@ function Contact() {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Dascalescu Rares,
+          <br />
+          Romania, Iasi
+          <span>rares.dascalescu@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+         <MapContainer center = {[47.1615598,27.5837814]} zoom = {7}>
+          <TileLayer url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position = {[47.1615598,27.5837814]}>
+          </Marker>
+         </MapContainer>
         </div>
       </div>
       <Loader type="ball-scale-ripple-multiple" />
